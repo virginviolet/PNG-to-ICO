71ca82da8dc6a5f874016ba7411225d4ae71ef56
virginviolet <148799361+virginviolet@users.noreply.github.com>
2024-09-05 23:11:14 +0200

    Converted batch script to PowerShell script

--- png_to_ico.ps1
+++ png_to_ico.ps1
@@ -1,48 +1,76 @@
-@ECHO off
-REM		Name :
-REM					PNG to ICO
-REM		Author :
-REM					▄▄▄▄▄▄▄  ▄ ▄▄ ▄▄▄▄▄▄▄
-REM					█ ▄▄▄ █ ██ ▀▄ █ ▄▄▄ █
-REM					█ ███ █ ▄▀ ▀▄ █ ███ █
-REM					█▄▄▄▄▄█ █ ▄▀█ █▄▄▄▄▄█
-REM					▄▄ ▄  ▄▄▀██▀▀ ▄▄▄ ▄▄
-REM					 ▀█▄█▄▄▄█▀▀ ▄▄▀█ █▄▀█
-REM					 █ █▀▄▄▄▀██▀▄ █▄▄█ ▀█
-REM					▄▄▄▄▄▄▄ █▄█▀ ▄ ██ ▄█
-REM					█ ▄▄▄ █  █▀█▀ ▄▀▀  ▄▀
-REM					█ ███ █ ▀▄  ▄▀▀▄▄▀█▀█
-REM					█▄▄▄▄▄█ ███▀▄▀ ▀██ ▄
-REM Console title
-TITLE PNG to ICO
-REM Script folder path
-SET directoryPath=%~dp0
-REM Console height / width
-MODE 65,30 | ECHO off
-ECHO.
-ECHO   -------------------------------------------------------------
-ECHO                            PNG to ICO :
-ECHO   -------------------------------------------------------------
-ECHO.
-REM First command line argument
-SET argPath=%1
-REM If first argument is a directory
-IF EXIST %argPath%\* (
-	REM ECHO Directory : %argPath%
-	REM Iterate through PNG, GIF, BMP, SVG and JPG files in directory
-	FOR %%f IN (%argPath%\*.png %argPath%\*.bmp %argPath%\*.gif %argPath%\*.jpg %argPath%\*.jpeg %argPath%\*.svg) DO (
-		REM Print file name (with extension)
-		ECHO - %%~nf%%~xf
-		REM Convert file to multi-resolution ICO
-		"%directoryPath%ImageMagick\magick.exe" "%%f" -resize 256x256^> -background none -gravity center -extent 256x256 -define icon:auto-resize=256,128,96,64,48,32,24,16 "%%~df%%~pf%%~nf.ico"
-	)
-REM If first argument is a file
-) ELSE (
-	REM ECHO File : %argPath%
-	FOR %%f IN (%argPath%) DO (
-		REM Print file name (with extension)
-		ECHO - %%~nf%%~xf
-		REM Convert file to multi-resolution ICO
-		"%directoryPath%ImageMagick\magick.exe" %argPath% -resize 256x256^> -background none -gravity center -extent 256x256 -define icon:auto-resize=256,128,96,64,48,32,24,16 "%%~df%%~pf%%~nf.ico"
-	)
-)
+<#
+ 	Name :
+				PNG to ICO
+	Author :
+				▄▄▄▄▄▄▄  ▄ ▄▄ ▄▄▄▄▄▄▄
+				█ ▄▄▄ █ ██ ▀▄ █ ▄▄▄ █
+				█ ███ █ ▄▀ ▀▄ █ ███ █
+				█▄▄▄▄▄█ █ ▄▀█ █▄▄▄▄▄█
+				▄▄ ▄  ▄▄▀██▀▀ ▄▄▄ ▄▄
+				 ▀█▄█▄▄▄█▀▀ ▄▄▀█ █▄▀█
+				 █ █▀▄▄▄▀██▀▄ █▄▄█ ▀█
+				▄▄▄▄▄▄▄ █▄█▀ ▄ ██ ▄█
+				█ ▄▄▄ █  █▀█▀ ▄▀▀  ▄▀
+				█ ███ █ ▀▄  ▄▀▀▄▄▀█▀█
+				█▄▄▄▄▄█ ███▀▄▀ ▀██ ▄ 
+#>
+
+# Console title
+$host.ui.RawUI.WindowTitle = "PNG to ICO"
+
+# Script directory path
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+# Console height / width
+$host.UI.RawUI.WindowSize = New-Object Management.Automation.Host.Size(65, 30)
+
+ECHO "`n"
+ECHO  " -------------------------------------------------------------"
+ECHO  "                          PNG to ICO :"
+ECHO  " -------------------------------------------------------------"
+ECHO "`n"
+
+# First command line argument
+$argPath = $args[0]
+
+# ImageMagick executable
+$magick = Join-Path $scriptDir -ChildPath "ImageMagick\magick.exe"
+
+# If first argument is a directory
+IF ([bool](Test-Path $argPath -PathType container)) {
+	
+	# ECHO "Directory : $argPath"
+
+	# Get images in directory
+	$dirContents = Join-Path $argPath -ChildPath '*'
+	$images = Get-ChildItem ($dirContents) -Include *.png, *.bmp, *.gif, *.jpg, *.jpeg, *.svg
+
+	# Iterate through the images
+	FOREACH ($i IN $images) {
+
+		# Print file name (with extension)
+		$fileName = $i.Name
+		ECHO "- $fileName"
+
+		# Convert file to multi-resolution ICO
+		$dir = Resolve-Path $argPath
+		$fileBaseName = (Get-Item -Path $argPath).BaseName # Name without extension
+		$icon = Join-Path -Path $dir -ChildPath "$fileBaseName.ico"
+		& $magick $argPath -resize 256x256^> -background none -gravity center -extent 256x256 -define icon:auto-resize=256,128,96,64,48,32,24,16 $icon
+	}
+# If first argument is a file
+} ELSE {
+	
+	# ECHO File : $argPath
+
+	# Print file name (with extension)
+	$fileName = (Get-Item -Path $argPath).Name
+	ECHO "- $fileName"
+
+	# Convert file to multi-resolution ICO
+	$dir = (Get-Item -Path $argPath).Directory
+	$fileBaseName = (Get-Item -Path $argPath).BaseName # Name without extension
+	$icon = Join-Path -Path $dir -ChildPath "$fileBaseName.ico"
+	echo $icon
+	& $magick $argPath -resize 256x256^> -background none -gravity center -extent 256x256 -define icon:auto-resize=256,128,96,64,48,32,24,16 $icon
+}
